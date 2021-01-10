8e3d2cd594c05caeae3420ddf7d0cc288f51ab8a
Harsh Singh <harshsngh07@gmail.com>
2021-01-11 00:22:51 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+console.log(`
+Harsh Singh
+
+I am a web developer with keen passion and experience in Problem Solving, Data Structures and Algorithms and C++.
+👩‍💻👨‍💻.
+
+Like to participate in hackathons, e-summits etc. and also have won a couple of hackathons.
+
+Passionate about new cutting edge technologies and open for relevant roles for the same.
+
+Currently, exploring the field of Problem Solving, Web Development and trying to devise solutions for common problems using programming.
+
+
+Connect with me 📲
+
+Linkedin - https://www.linkedin.com/in/harshsngh07/
+Github   - https://github.com/Harshsngh07
+Twitter  - https://twitter.com/Harshsngh07
+
+Thanks for visiting. 
+`);
